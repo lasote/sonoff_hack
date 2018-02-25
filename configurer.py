818feb3e918ff8d3302f85a9804d5c46c5119965
Luis Martinez de Bartolome <luism@jfrog.com>
2018-02-25 10:46:51 +0100

    configurer

---
+++ configurer.py
@@ -0,0 +1,35 @@
+import requests
+from requests import ConnectTimeout
+
+
+def get_credentials():
+    try:
+        ret = requests.get("http://10.10.7.1/device", timeout=5 )
+        if ret.status_code == 200:
+            return ret.json()
+        return None
+    except ConnectTimeout:
+        return None
+
+
+if __name__ == "__main__":
+
+    print("\n***********************************************************\n\n"
+          "- Connect the sonoff and press the button for few seconds\n"
+          "- Scan your wifi networks and connect to ITEAD-10000XXX\n"
+          "- Use password: 12345678\n"
+          "\n***********************************************************")
+
+    input("Press Enter to continue.")
+
+    print("Waiting for the device...")
+    while True:
+        credentials = get_credentials()
+        if credentials:
+            break
+        else:
+            print("Not connected...")
+
+    print("Connected to the device!! %s" % credentials)
+
+
